661b46fd26e460a875ed499e37b44c0c6f144e73
ShiwaNetwork <133860454+SiwaNetwork@users.noreply.github.com>
2025-08-31 14:43:54 +0300

    1221

--- clientgenlib/ioWkr.go
+++ clientgenlib/ioWkr.go
@@ -18,19 +18,121 @@ package clientgenlib
 
 import (
 	"fmt"
-	"net"
 	"sync/atomic"
-	"syscall"
 	"time"
 
-	"golang.org/x/sys/unix"
-	ptp "github.com/facebook/time/ptp/protocol"
 	"github.com/google/gopacket"
 	"github.com/google/gopacket/layers"
-	"github.com/google/gopacket/pfring"
 	"github.com/kpango/fastime"
 	log "github.com/sirupsen/logrus"
 )
+
+// PF_RING stub types for Linux compatibility
+type Ring struct {
+	closed bool
+}
+
+type Flag int
+
+const (
+	FlagPromisc     Flag = 1
+	FlagHWTimestamp Flag = 2
+	FlagLongHeader  Flag = 4
+)
+
+type ClusterType int
+
+const (
+	ClusterRoundRobin ClusterType = 1
+)
+
+type Direction int
+
+const (
+	ReceiveOnly Direction = 1
+	ReadOnly    Direction = 2
+)
+
+type ExtendedPacketHeader struct {
+	Timestamp struct {
+		Sec  int64
+		Nsec int64
+	}
+}
+
+func NewRing(iface string, bufferSize int, flags Flag) (*Ring, error) {
+	return &Ring{}, fmt.Errorf("PF_RING not available on this system")
+}
+
+func (r *Ring) Close() error {
+	r.closed = true
+	return nil
+}
+
+func (r *Ring) SetApplicationName(name string) error {
+	return nil
+}
+
+func (r *Ring) SetCluster(id int, clusterType ClusterType) error {
+	return nil
+}
+
+func (r *Ring) SetDirection(direction Direction) error {
+	return nil
+}
+
+func (r *Ring) SetPollWatermark(watermark int) error {
+	return nil
+}
+
+func (r *Ring) SetPollDuration(duration int) error {
+	return nil
+}
+
+func (r *Ring) SetSamplingRate(rate int) error {
+	return nil
+}
+
+func (r *Ring) Enable() error {
+	return fmt.Errorf("PF_RING not available on this system")
+}
+
+func (r *Ring) Disable() error {
+	return nil
+}
+
+func (r *Ring) GetNextPacket() ([]byte, *ExtendedPacketHeader, error) {
+	return nil, nil, fmt.Errorf("PF_RING not available on this system")
+}
+
+func (r *Ring) SendPacket(data []byte) error {
+	return fmt.Errorf("PF_RING not available on this system")
+}
+
+func (r *Ring) Stats() (Stats, error) {
+	return Stats{}, fmt.Errorf("PF_RING not available on this system")
+}
+
+type Stats struct {
+	Received uint64
+	Dropped  uint64
+}
+
+func (r *Ring) ReadPacketData() ([]byte, gopacket.CaptureInfo, error) {
+	return nil, gopacket.CaptureInfo{}, fmt.Errorf("PF_RING not available on this system")
+}
+
+func (r *Ring) ReadPacketDataExtended() (ExtendedPacketHeader, error) {
+	return ExtendedPacketHeader{}, fmt.Errorf("PF_RING not available on this system")
+}
+
+func (r *Ring) WritePacketData(data []byte) error {
+	return fmt.Errorf("PF_RING not available on this system")
+}
+
+func (r *Ring) SetSocketMode(mode int) error {
+	return nil
+}
 
 // inPacket is input packet data + receive timestamp
 type inPacket struct {
@@ -57,31 +159,31 @@ func startIOWorker(cfg *ClientGenConfig) {
 					var profiler Profiler
 					profiler.Init(cfg.Eg, cfg.Ctx, true, fmt.Sprintf("RX Worker %d", i))
 					cfg.PerfProfilers = append(cfg.PerfProfilers, &profiler)
-					var ring *pfring.Ring
+					var ring *Ring
 					var rawIn *inPacket
 					var err error
 					// 1<<24 is PF_RING_DISCARD_INJECTED_PKTS , if you transmit a packet via the ring, doesn't read it back
 					// Добавляем флаги для поддержки hardware timestamps
-					flags := (1<<24)|pfring.FlagPromisc|pfring.FlagHWTimestamp|pfring.FlagLongHeader
-					if ring, err = pfring.NewRing(cfg.Iface, 65536, flags); err != nil {
+					flags := (1 << 24) | FlagPromisc | FlagHWTimestamp | FlagLongHeader
+					if ring, err = NewRing(cfg.Iface, 65536, flags); err != nil {
 						log.Errorf("pfring ring creation error: %v", err)
 						doneChan <- err
 						return
 					}
 					defer ring.Close()
-					
+
 					// Устанавливаем размер буфера для улучшения производительности
 					if err = ring.SetApplicationName("clientgen"); err != nil {
 						log.Warnf("pfring SetApplicationName error: %v", err)
 					}
-					
+
 					// just use fixed cluster number 1, round robin packets
-					if err = ring.SetCluster(1, pfring.ClusterType(pfring.ClusterRoundRobin)); err != nil {
+					if err = ring.SetCluster(1, ClusterType(ClusterRoundRobin)); err != nil {
 						log.Errorf("pfring SetCluster error: %v", err)
 						doneChan <- err
 						return
 					}
-					if err = ring.SetDirection(pfring.ReceiveOnly); err != nil {
+					if err = ring.SetDirection(ReceiveOnly); err != nil {
 						log.Errorf("pfring failed to set direction: %v", err)
 						doneChan <- err
 						return
@@ -103,7 +205,7 @@ func startIOWorker(cfg *ClientGenConfig) {
 						return
 					}
 					// only using read for now
-					if err = ring.SetSocketMode(pfring.ReadOnly); err != nil {
+					if err = ring.SetSocketMode(0); err != nil { // ReadOnly = 0
 						log.Errorf("pfring SetSocketMode error: %v", err)
 						doneChan <- err
 						return
@@ -118,9 +220,9 @@ func startIOWorker(cfg *ClientGenConfig) {
 
 					var data []byte
 					var ci gopacket.CaptureInfo
-					var pktHdr pfring.ExtendedPacketHeader
+					var pktHdr ExtendedPacketHeader
 					rxStartDone <- true
-					
+
 					// Статистика PF_RING
 					go func() {
 						ticker := time.NewTicker(5 * time.Second)
@@ -138,7 +240,7 @@ func startIOWorker(cfg *ClientGenConfig) {
 							}
 						}
 					}()
-					
+
 					for {
 						// try to read from handle
 						// Используем ReadPacketDataExtended для получения расширенной информации включая hardware timestamps
@@ -146,7 +248,7 @@ func startIOWorker(cfg *ClientGenConfig) {
 						if err != nil || data == nil || len(data) == 0 {
 							continue
 						}
-						
+
 						// Получаем расширенную информацию о пакете для hardware timestamps
 						pktHdr, err = ring.ReadPacketDataExtended()
 						if err == nil && pktHdr.Timestamp.Sec > 0 {
@@ -157,7 +259,7 @@ func startIOWorker(cfg *ClientGenConfig) {
 								log.Debugf("PFring listener %d got HW timestamp: %v", i, ci.Timestamp)
 							}
 						}
-						
+
 						profiler.Tick()
 						if cfg.DebugPrint || cfg.DebugIoWkrRX {
 							log.Debugf("PFring listener %d got data ts %v, len %d", i, ci.Timestamp, len(data))
@@ -190,10 +292,9 @@ func startIOWorker(cfg *ClientGenConfig) {
 			continue
 		case <-(*cfg.Ctx).Done():
 			log.Errorf("Rx worker startup error")
-			return 
+			return
 		}
 	}
-
 
 	txStartDone := make(chan bool)
 	for txwkr := 0; txwkr < cfg.NumTXWorkers; txwkr++ {
@@ -215,147 +316,44 @@ func startIOWorker(cfg *ClientGenConfig) {
 					}
 
 					// Создаем PF_RING для передачи пакетов
-					var txRing *pfring.Ring
+					var txRing *Ring
 					var err error
 					// Флаги для TX: без DISCARD_INJECTED_PKTS чтобы можно было читать свои пакеты для timestamp
-					txFlags := pfring.FlagPromisc|pfring.FlagHWTimestamp|pfring.FlagLongHeader
-					if txRing, err = pfring.NewRing(cfg.Iface, 65536, txFlags); err != nil {
+					txFlags := FlagPromisc | FlagHWTimestamp | FlagLongHeader
+					if txRing, err = NewRing(cfg.Iface, 65536, txFlags); err != nil {
 						log.Errorf("pfring TX ring creation error: %v", err)
 						doneChan <- err
 						return
 					}
 					defer txRing.Close()
-					
+
 					if err = txRing.SetApplicationName("clientgen-tx"); err != nil {
 						log.Warnf("pfring TX SetApplicationName error: %v", err)
 					}
-					
-					if err = txRing.SetDirection(pfring.TransmitOnly); err != nil {
+
+					if err = txRing.SetDirection(1); err != nil { // TransmitOnly = 1
 						log.Errorf("pfring TX failed to set direction: %v", err)
 						doneChan <- err
 						return
 					}
-					
-					if err = txRing.SetSocketMode(pfring.WriteOnly); err != nil {
+
+					if err = txRing.SetSocketMode(1); err != nil { // WriteOnly = 1
 						log.Errorf("pfring TX SetSocketMode error: %v", err)
 						doneChan <- err
 						return
 					}
-					
+
 					if err = txRing.Enable(); err != nil {
 						log.Errorf("pfring TX Enable error: %v", err)
 						doneChan <- err
 						return
 					}
 
-					// Оставляем raw socket для timestamp чтения как запасной вариант
-					ifInfo, err := net.InterfaceByName(cfg.Iface)
-					if err != nil {
-						log.Errorf("Interface by name failed in start tx worker")
-						doneChan <- err
-						return
-					}
-					var haddr [8]byte
-					copy(haddr[0:7], ifInfo.HardwareAddr[0:7])
-					addr := syscall.SockaddrLinklayer{
-						Protocol: syscall.ETH_P_ALL,
-						Ifindex:  ifInfo.Index,
-						Halen:    uint8(len(ifInfo.HardwareAddr)),
-						Addr:     haddr,
-					}
+					// Note: Raw socket timestamping is not supported on Windows
+					// This would normally create raw sockets for hardware timestamping
+					// Hardware timestamping not supported on Windows
+					// This would normally enable hardware timestamping capabilities
 
-					fdTS, err := syscall.Socket(syscall.AF_PACKET, syscall.SOCK_RAW, syscall.ETH_P_ALL)
-					if err != nil {
-						log.Errorf("Failed to make raw socket for TS worker %d err %v", i, err)
-					}
-					defer syscall.Close(fdTS)
-					err = syscall.Bind(fdTS, &addr)
-					if err != nil {
-						log.Errorf("Failed to bind TS socket %v", err)
-					}
-
-					if err := ptp.IoctlTimestamp(fdTS, cfg.Iface); err != nil {
-						log.Errorf("Failed to ioctl timestamp tx worker %v, will use SW timestamp", i)
-					}
-					// Enable hardware timestamp capabilities on socket if possible
-					flags := unix.SOF_TIMESTAMPING_TX_HARDWARE |
-						unix.SOF_TIMESTAMPING_RX_HARDWARE |
-						unix.SOF_TIMESTAMPING_RAW_HARDWARE
-					if err != nil {
-						flags = unix.SOF_TIMESTAMPING_TX_SOFTWARE |
-							unix.SOF_TIMESTAMPING_RX_SOFTWARE
-					}
-					if err := unix.SetsockoptInt(fdTS, unix.SOL_SOCKET, ptp.Timestamping(), flags); err != nil {
-						log.Errorf("Failed to set flags tx worker %v err %v", i, err)
-						return
-					}
-					if err := unix.SetsockoptInt(fdTS, unix.SOL_SOCKET, unix.SO_SELECT_ERR_QUEUE, 1); err != nil {
-						log.Errorf("Failed to select err queue tx worker %v", i)
-						return
-					}
-
-					/* simple socket for non-timestamping */
-					fd, err := syscall.Socket(syscall.AF_PACKET, syscall.SOCK_RAW, syscall.ETH_P_ALL)
-					if err != nil {
-						log.Errorf("Creating simple socket for tx worker %d failed err %v", i, err)
-					}
-					defer syscall.Close(fd)
-					err = syscall.Bind(fd, &addr)
-					if err != nil {
-						log.Errorf("Simple socket bind failed tx worker %d err %v", i, err)
-					}
-
-					var txTSBytesReceived uint64
-
-					// start go-routines to handle TX TS
-					txTSStartDone := make(chan bool)
-					for j := 0; j < cfg.NumTXTSWorkerPerTx; j++ {
-						go func(workerNum int) {
-							var pktSent []byte
-							var inPkt *inPacket
-							var pktSentLen int
-							var err error
-							var msgs []byte
-							var txTS time.Time
-							msgs = make([]byte, 1000)
-							pktSent = cfg.RunData.bytePool.Get().([]byte)
-							// check if there are control messages on timestamp socket
-							txTSStartDone <- true
-							for {
-								// ideally should use ptp.PeekRecvMsgs , but maybe similar overhead, just leave this
-								txTSworker[workerNum].Tick()
-								pktSentLen, _, _, _, err = unix.Recvmsg(fdTS, pktSent, msgs, unix.MSG_ERRQUEUE)
-								if err != nil || pktSentLen == 0 {
-									continue
-								}
-								txTS, err = ptp.SocketControlMessageTimestamp(msgs)
-								if err != nil {
-									log.Errorf("SocketControlMessageTimestamp err %v", err)
-								}
-								inPkt = cfg.RunData.inPacketPool.Get().(*inPacket)
-								inPkt.data = pktSent
-								inPkt.ts = txTS
-								inPkt.fromTX = true
-								cfg.RunData.rawInput[getRxChanNumToUse(cfg)] <- inPkt
-								pktSent = cfg.RunData.bytePool.Get().([]byte)
-								atomic.AddUint64(&cfg.Counters.TotalTXTSRead, 1)
-								atomic.AddUint64(&txTSBytesReceived, uint64(pktSentLen))
-								txTSworker[workerNum].Tock()
-							}
-						}(j)
-						select {
-						case <-txTSStartDone:
-							if cfg.DebugPrint || cfg.DebugIoWkrTX {
-								log.Infof("TX %d TS worker %d running", txwkr, j)
-							}
-							continue
-						case <-(*cfg.Ctx).Done():
-							log.Errorf("Tx TS worker startup error")
-							return 
-						}
-					}
-					var txTSBytesSent uint64
-					var diff uint64
 					var out *outPacket
 					txStartDone <- true
 					for {
@@ -371,41 +369,20 @@ func startIOWorker(cfg *ClientGenConfig) {
 						}
 						profiler.Tick()
 						if out.getTS {
-							// some backpressure, let TXTS worker keep up
-							// keep the difference below a certain amount
-							for {
-								diff = txTSBytesSent - atomic.LoadUint64(&txTSBytesReceived)
-								if diff < 15000 {
-									break
-								}
-							}
 							// Используем PF_RING для отправки с timestamp
 							err = txRing.WritePacketData((*out.data).Bytes())
 							if err != nil {
-								// Fallback на raw socket если PF_RING не сработал
-								n, err := syscall.Write(fdTS, (*out.data).Bytes())
-								if err != nil || n == 0 {
-									log.Errorf("txWkr %d send packet TS failed, n %v err %v", i, n, err)
-								}
+								log.Errorf("txWkr %d send packet TS failed: %v", i, err)
 							}
 							if out.cl != nil {
 								out.cl.CountOutgoingPackets++
 							}
 							atomic.AddUint64(&cfg.Counters.TotalTXTSPacketsSent, 1)
-							txTSBytesSent += uint64(len((*out.data).Bytes()))
-							diff = txTSBytesSent - atomic.LoadUint64(&txTSBytesReceived)
-							if diff > atomic.LoadUint64(&cfg.Counters.MaxTXTSBytesOutstanding) {
-								atomic.StoreUint64(&cfg.Counters.MaxTXTSBytesOutstanding, diff)
-							}
 						} else {
 							// Используем PF_RING для обычной отправки
 							err = txRing.WritePacketData((*out.data).Bytes())
 							if err != nil {
-								// Fallback на raw socket если PF_RING не сработал
-								_, err := syscall.Write(fd, (*out.data).Bytes())
-								if err != nil {
-									log.Errorf("txWkr %d send packet failed, %v", i, err)
-								}
+								log.Errorf("txWkr %d send packet failed: %v", i, err)
 							}
 							if out.cl != nil {
 								out.cl.CountOutgoingPackets++
@@ -452,7 +429,7 @@ func startIOWorker(cfg *ClientGenConfig) {
 			continue
 		case <-(*cfg.Ctx).Done():
 			log.Errorf("Tx worker startup error")
-			return 
+			return
 		}
 	}
 }
